a16f56abd72ffc580add6280f359b6322d7b13cf
Ammar Faizi <ammarfaizi2@gmail.com>
2019-05-16 23:05:13 +0700

    Fix downloader

--- src/ZippyShare/ZippyShare.php
+++ src/ZippyShare/ZippyShare.php
@@ -2,6 +2,10 @@
 
 namespace ZippyShare;
 
+use Error;
+use stdClass;
+use Exception;
+
 /**
  * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
  * @license MIT
@@ -30,6 +34,16 @@ final class ZippyShare
 	private $fileUrl;
 
 	/**
+	 * @var string
+	 */
+	private $proxy = -1;
+
+	/**
+	 * @var int
+	 */
+	private $proxyType = CURLPROXY_SOCKS5;
+
+	/**
 	 * @var resource
 	 */
 	private static $logHandle = -1;
@@ -40,7 +54,7 @@ final class ZippyShare
 	public function __construct(string $url)
 	{
 		$this->url = $url;
-		$this->cookieFile = defined("ZIPPYSHARE_COOKIEFILE_DIR") ? ZIPPYSHARE_COOKIEFILE_DIR."/".sha1($this->url) : __DIR__.sha1($this->url);
+		$this->cookieFile = defined("ZIPPYSHARE_COOKIEFILE_DIR") ? ZIPPYSHARE_COOKIEFILE_DIR."/".sha1($this->url) : __DIR__."/".sha1($this->url);
 
 		if (!is_resource(self::$logHandle)) {
 			if (defined("ZIPPYSHARE_LOG_HANDLE") && is_resource("ZIPPYSHARE_LOG_HANDLE")) {
@@ -52,21 +66,39 @@ final class ZippyShare
 	}
 
 	/**
-	 * @return bool
+	 * Destructor.
 	 */
-	public function exec(): bool
+	public function __destruct()
 	{
-		if ($this->visitPage()) {
-
-		}
+		// file_exists($this->cookieFile) and unlink($this->cookieFile);
 	}
 
 	/**
 	 * @return bool
 	 */
-	private function visitPage(): bool
+	public function exec(): bool
 	{
+		try {
+			if ($this->visitPage()) {
+				$this->downloadFile();
+				return true;
+			}
+		} catch (Exception $e) {
+			self::log("An error occured: %s", $e->getMessage());
+		}
 
+		return false;
+	}
+
+	/**
+	 * @param string $proxy
+	 * @param int    $proxyType
+	 * @return void
+	 */
+	public function setProxy(string $proxy, int $proxyType = CURLPROXY_SOCKS5): void
+	{
+		$this->proxy = $proxy;
+		$this->proxyType = $proxyType;
 	}
 
 	/**
@@ -74,16 +106,87 @@ final class ZippyShare
 	 */
 	public function outFile(string $outFile): void
 	{
-		$this->outFile = $file;
+		$this->outFile = $outFile;
+	}
+
+	/**
+	 * @return void
+	 */
+	private function downloadFile(): void
+	{
+		self::log("Downloading file...");
+
+		$handle = fopen($this->outFile, "w");
+
+		$o = $this->curl($this->fileUrl,
+			[
+				CURLOPT_RETURNTRANSFER => false,
+				CURLOPT_WRITEFUNCTION => function ($ch, $str) use ($handle) {
+					return fwrite($handle, $str);
+				}
+			]
+		);
+
+		if ($o->error) {
+			throw new Exception("Download error: ({$o->errno}) {$o->error}");
+		}
+
+		fflush($handle);
+		fclose($handle);
+	}
+
+	/**
+	 * @throws \Exception
+	 * @return bool
+	 */
+	private function visitPage(): bool
+	{
+		$o = $this->curl($this->url);
+		// print $o->out;
+		// file_put_contents("test.tmp", $o->out);
+		// $o = new stdClass;
+		// $o->out = file_get_contents("test.tmp");
+
+		if (preg_match(
+			"/(?:<script type=\"text\/javascript\">[\n\s]+?var a = )(\d+)(?:;[\n\s]+?var b = )(\d+)(?:;.+?dlbutton'\).href = \")(.+?)(?:\"\+\((.+?)\)\+\")(.+?)(?:\")/si",
+			$o->out,
+			$m
+		)) {
+			$a = (int)$m[1];
+			$b = (int)$m[2];
+
+			$m[4] = str_replace(["a", "b"], ["\$a", "\$b"], $m[4]);
+
+			var_dump($a, $b, $m[4]);
+
+			$a = (int)floor(((int)$m[1]) / 3);
+
+			try {
+				eval("\$m[4] = {$m[4]};");
+			} catch (Error $e) {
+				throw new Exception("Invalid evaluation");
+			}
+
+			$serverNum = "";
+			if (preg_match("/www(\d+)/", $this->url, $mm)) {
+				$serverNum = $mm[1];
+			}
+
+			$this->fileUrl = "https://www{$serverNum}.zippyshare.com/".ltrim($m[3], "/").$m[4].$m[5];
+
+			return true;
+		}
+
+		throw new Exception("Couldn't find the file link");
 	}
 
 
 	/**
 	 * @param string $url
 	 * @param array  $opt
-	 * @return array
+	 * @return stdClass
 	 */
-	private function curl(string $url, array $opt = []): array
+	private function curl(string $url, array $opt = []): stdClass
 	{
 		self::log("Preparing curl to %s...", $url);
 
@@ -102,6 +205,11 @@ start_curl:
 			CURLOPT_COOKIEFILE => $this->cookieFile
 		];
 
+		if ($this->proxy !== -1) {
+			$optf[CURLOPT_PROXY] = $this->proxy;
+			$optf[CURLOPT_PROXYTYPE] = $this->proxyType;
+		}
+
 		foreach ($opt as $k => $v) {
 			$optf[$k] = $v;
 		}
@@ -110,14 +218,16 @@ start_curl:
 
 		self::log("Fetching %s...", $url);
 
-		$out = curl_exec($ch);
-		$err = curl_error($ch);
-		$ern = curl_errno($ch);
-		$info = curl_getinfo($ch);
+		$o = new stdClass;
+
+		$o->out = curl_exec($ch);
+		$o->err = curl_error($ch);
+		$o->ern = curl_errno($ch);
+		$o->info = curl_getinfo($ch);
 		curl_close($ch);
 
-		if ($err) {
-			self::log("There was an error occured when accessing %s: (%d) %s.", $url, $ern, $err);
+		if ($o->err) {
+			self::log("There was an error occured when accessing %s: (%d) %s.", $url, $o->ern, $o->err);
 			if (!$retried) {
 				self::log("Retrying to fetch %s...", $url);
 				$retried = true;
@@ -126,15 +236,10 @@ start_curl:
 				self::log("Aborting fetch %s...", $url);
 			}
 		} else {
-			self::log("Fetch completed with http_code: %d", $info["http_code"]);
+			self::log("Fetch completed with http_code: %d", $o->info["http_code"]);
 		}
 
-		return [
-			"out" => $out,
-			"error" => $err,
-			"errno" => $ern,
-			"info" => $info
-		];
+		return $o;
 	}
 
 	/**
